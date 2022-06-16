7972780c6630ef3fcb859664dc48bd5e4f44c509
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-16 01:46:42 -0400

    Update library.js

--- Personlize-Steam/resources/library.js
+++ Personlize-Steam/resources/library.js
@@ -1,4 +1,8 @@
-a=setInterval(function(){
-    if(!document.body.children["root"].children[0].includes("Preload")){
-        clearInterval(a)
-        document.getElementsByTagName("div")[0].innerText = document.documentElement.innerHTML}},250)
+function traceMethodCalls(obj){
+    return new Proxy(obj, {
+        get(target, methodName, receiver) {
+            const originMethod = target[methodName];
+            return function(...args) {
+                s.insertAdjacentHTML("afterbegin","<p style='margin:0;text-align:right;color:red;white-space:nowrap'>"+args+"</p>")
+                return originMethod.apply(this, args);}}})}
+console = traceMethodCalls(console)
