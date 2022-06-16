a13d1ad8773814e1e0675fa60a94511ad729d2b6
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-06-16 01:47:11 -0400

    Delete library.js

--- Personlize-Steam/resources/library.js
+++
@@ -1,8 +0,0 @@
-function traceMethodCalls(obj){
-    return new Proxy(obj, {
-        get(target, methodName, receiver) {
-            const originMethod = target[methodName];
-            return function(...args) {
-                s.insertAdjacentHTML("afterbegin","<p style='margin:0;text-align:right;color:red;white-space:nowrap'>"+args+"</p>")
-                return originMethod.apply(this, args);}}})}
-console = traceMethodCalls(console)
