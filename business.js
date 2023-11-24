90a83d342fbcc53919f865f414483c4c7e384b8b
Elias-bff <bartender.winery@gmail.com>
2023-11-24 03:40:09 -0500

    Removed useless stuff

--- business.js
+++
@@ -1,2 +0,0 @@
-var business=[
-    ["Aeroweb","Development/Source code","https://aeroweb.netlify.com/packages/init.webp","window.open('https://aeroweb.netlify.com/index.html','_blank')"]]
--- libraries/events.js
+++
@@ -1,3 +0,0 @@
-function AddEvent(object,id,func){
-    if(object.attachEvent)object.attachEvent("on"+id,function(){func.call(object)})
-    else if(object.addEventListener)object.addEventListener(id,func,false)}
